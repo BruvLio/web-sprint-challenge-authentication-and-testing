5a9b656b4e03f292a80d553ecd0e90875cac97ba
BruvLio <lionel.mbayu@gmail.com>
2023-05-26 18:19:17 -0500

    wip

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,15 @@
-// Write your tests here
+// Write your tests here\\
+// const db = require('../data/dbConfig');
+// const request = require('supertest');
+// const server = require('../api/server');
+
+// beforeAll(async () => {
+// 	await db.migrate.rollback();
+// 	await db.migrate.latest();
+// // });
+
+// describe('[POST] /api/users')
+
 test('sanity', () => {
 	expect(true).toBe(true);
 });
